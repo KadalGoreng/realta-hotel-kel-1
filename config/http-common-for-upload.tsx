7f7cde9a509c6ed98b074166dee4e88ef5f0014d
AuroSilalahi <61122013+AuroSilalahi@users.noreply.github.com>
2023-06-06 14:13:31 +0700

    Delete config directory

--- config/http-common-for-upload.tsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "http://localhost:3005",
-  headers: {
-    "Content-Type": "multipart/form-data",
-    // 'Authorization': token
-  },
-});
